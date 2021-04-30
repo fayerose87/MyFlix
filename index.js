3c89e4c20741153d01b98ead81931c8f54302dd3
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-04-29 20:35:35 -0500

    Added top 10 movies

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 const express = require("express");
+morgan = require("morgan");
+
 const app = express();
+
+let topMovies = [
+  {
+    title: "Inception",
+  },
+  {
+    title: "Interstellar",
+  },
+  {
+    title: "Memento",
+  },
+  {
+    title: "12 Monkeys",
+  },
+  {
+    title: "Eternal Sunshine Of The Spotless Mind",
+  },
+  {
+    title: "The Shawshank Redemption",
+  },
+  {
+    title: "Forrest Gump",
+  },
+  {
+    title: "Fight Club",
+  },
+  {
+    title: "The Lord Of The Rings: The Return Of The King",
+  },
+  {
+    title: "The Dark Night",
+  },
+];
+
+app.use(morgan("common"));
 
 // GET requests
 app.get("/", (req, res) => {
