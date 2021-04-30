00a5153b54f4ed317a43560e202c287ecc465b1f
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-04-29 19:56:17 -0500

    Require express and created GET routes

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const express = require("express");
 const app = express();
+
+// GET requests
+app.get("/", (req, res) => {
+  res.send("Welcome to my MyFlix!");
+});
+
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/documentation.html", { root: __dirname });
+});
+
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
+});
+
+//Listen for requests
+app.listen(8080, () => {
+  console.log("This app is listening on Port 8080.");
+});
