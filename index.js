7156fc8bcfaa48d6809f1bd76ff84eda3948f34b
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-04-29 21:49:42 -0500

    Added express.static

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 const express = require("express");
+morgan = require("morgan");
+
 const app = express();
 
-// GET requests
+let topMovies = [
+  {
+    title: "Inception",
+  },
+  {
+    title: "Interstellar",
+  },
+  {
+    title: "Memento",
+  },
+  {
+    title: "12 Monkeys",
+  },
+  {
+    title: "Eternal Sunshine Of The Spotless Mind",
+  },
+  {
+    title: "The Shawshank Redemption",
+  },
+  {
+    title: "Forrest Gump",
+  },
+  {
+    title: "Fight Club",
+  },
+  {
+    title: "The Lord Of The Rings: The Return Of The King",
+  },
+  {
+    title: "The Dark Night",
+  },
+];
+
+app.use(morgan("common"));
+
+//GET requests
 app.get("/", (req, res) => {
   res.send("Welcome to my MyFlix!");
 });
 
-app.get("/documentation", (req, res) => {
-  res.sendFile("public/documentation.html", { root: __dirname });
-});
-
 app.get("/movies", (req, res) => {
   res.json(topMovies);
+});
+
+app.use(express.static("Public"));
+
+//Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 //Listen for requests
