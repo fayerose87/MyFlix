7d8bc92264214822ab7c26278c5941be47d3ccc0
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-05-23 21:14:34 -0500

    Formatting fixes

--- index.js
+++ index.js
@@ -96,26 +96,26 @@ app.post("/users", (req, res) => {
 });
 
 // Get all users
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
   Users.find()
     .then((users) => {
       res.status(201).json(users);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error: ' + err);
+      res.status(500).send("Error: " + err);
     });
 });
 
 // Get a user by username
-app.get('/users/:Username', (req, res) => {
+app.get("/users/:Username", (req, res) => {
   Users.findOne({ Username: req.params.Username })
     .then((user) => {
       res.json(user);
     })
     .catch((err) => {
       console.error(err);
-      res.status(500).send('Error: ' + err);
+      res.status(500).send("Error: " + err);
     });
 });
 
