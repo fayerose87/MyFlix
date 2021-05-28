cfe01542ff9ed9d34f29cf1cb4cd5a7ebce6d66d
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-05-27 21:18:44 -0500

    Hash password when loggin in

--- passport.js
+++ passport.js
@@ -1,17 +1,17 @@
-const passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy,
-  Models = require('./models.js'),
-  passportJWT = require('passport-jwt');
+const passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy,
+  Models = require("./models.js"),
+  passportJWT = require("passport-jwt");
 
 let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
-  usernameField: 'Username',
-  passwordField: 'Password'
+  usernameField: "Username",
+  passwordField: "Password"
 }, (username, password, callback) => {
-  console.log(username + '  ' + password);
+  console.log(username + "  " + password);
   Users.findOne({ Username: username }, (error, user) => {
     if (error) {
       console.log(error);
@@ -19,18 +19,23 @@ passport.use(new LocalStrategy({
     }
 
     if (!user) {
-      console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password.'});
+      console.log("incorrect username");
+      return callback(null, false, {message: "Incorrect username or password."});
     }
 
-    console.log('finished');
+    if (!user.validatePassword(password)) {
+      console.log("incorrect password");
+      return callback(null, false, {message: "Incorrect password"});
+    }
+
+    console.log("finished");
     return callback(null, user);
   });
 }));
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
+  secretOrKey: "your_jwt_secret"
 }, (jwtPayload, callback) => {
   return Users.findById(jwtPayload._id)
     .then((user) => {
