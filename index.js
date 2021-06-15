566998e4874d4ce79d361e41ad91dda84a49c25b
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-06-14 19:58:54 -0500

    Temporarily remove authentication middleware

--- index.js
+++ index.js
@@ -37,9 +37,7 @@ app.get("/", (req, res) => {
 
 //Get a list of all movies
 app.get(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
+  "/movies", (req, res) => {
     Movies.find()
       .then((movies) => {
         res.status(201).json(movies);
